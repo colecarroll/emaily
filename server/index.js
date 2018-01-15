414c7ea7f7ca8b2326caaa06234c1c474c13c1a6
Cole Carroll <colecarroll2@gmail.com>
2018-01-15 13:31:13 -0700

    trying to work out internal server error bug

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const keys = require("./config/keys");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
 const bodyParser = require("body-parser");
-require("./models/user");
+const keys = require("./config/keys");
+require("./models/User");
 require("./services/passport");
 
 mongoose.connect(keys.mongoURI);
@@ -25,13 +25,15 @@ require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
 if (process.env.NODE_ENV === "production") {
-  // express will serve up production assets like main.js file or main.css file
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
   app.use(express.static("client/build"));
 
-  //express will serve up index.html file if it doesn't recognize route
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
   const path = require("path");
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(_dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
